05dc5e9501fce2dc4d6f3f25617ed74a13ffef8c
thiagorabaioli <tfrabaioli@outlook.com>
2023-08-01 12:16:06 +0100

    first commit

---
+++ src/main/java/tfrabaioli/DsListbackend/DsListBackendApplication.java
@@ -0,0 +1,13 @@
+package tfrabaioli.DsListbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsListBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsListBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tfrabaioli/DsListbackend/DsListBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package tfrabaioli.DsListbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsListBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
