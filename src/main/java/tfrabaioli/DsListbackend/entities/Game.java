2fc410c9618d4d2d2ed393f4c884faadd215a0cd
thiagorabaioli <tfrabaioli@outlook.com>
2023-08-01 14:30:09 +0100

    seed db

--- src/main/java/tfrabaioli/DsListbackend/entities/Game.java
+++ src/main/java/tfrabaioli/DsListbackend/entities/Game.java
@@ -21,93 +21,130 @@ public class Game implements Serializable{
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String title;
+	
 	@Column(name = "game_year")
 	private Integer year;
 	private String genre;
-	private String platform;
-	private String imgUrl;
+	private String platforms;
+	private String score;
+	private String img_url;
+	
+	@Column(columnDefinition = "TEXT")
 	private String shortDescription;
+	
+	@Column(columnDefinition = "TEXT")
 	private String longDescription;
 	
-	
+
 	public Game() {};
 	
-	public Game(Long id, String title, Integer year, String genre, String platform, String imgUrl,
-			String shortDescription, String longDescription) {
+
+	public Game(Long id, String title, String score, Integer year, String genre, String platforms, String img_url,
+			String short_description, String long_description) {
 		super();
 		this.id = id;
 		this.title = title;
+		this.score = score;
 		this.year = year;
 		this.genre = genre;
-		this.platform = platform;
-		this.imgUrl = imgUrl;
-		this.shortDescription = shortDescription;
-		this.longDescription = longDescription;
+		this.platforms = platforms;
+		this.img_url = img_url;
+		this.shortDescription = short_description;
+		this.longDescription = long_description;
 	}
+
 
 	public Long getId() {
 		return id;
 	}
 
+
+	
+	
+	public String getScore() {
+		return score;
+	}
+
+
+	public void setScore(String score) {
+		this.score = score;
+	}
+
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 
 	public String getTitle() {
 		return title;
 	}
 
+
 	public void setTitle(String title) {
 		this.title = title;
 	}
+
 
 	public Integer getYear() {
 		return year;
 	}
 
+
 	public void setYear(Integer year) {
 		this.year = year;
 	}
+
 
 	public String getGenre() {
 		return genre;
 	}
 
+
 	public void setGenre(String genre) {
 		this.genre = genre;
 	}
 
-	public String getPlatform() {
-		return platform;
+
+	public String getPlatforms() {
+		return platforms;
 	}
 
-	public void setPlatform(String platform) {
-		this.platform = platform;
+
+	public void setPlatforms(String platforms) {
+		this.platforms = platforms;
 	}
 
-	public String getImgUrl() {
-		return imgUrl;
+
+	public String getImg_url() {
+		return img_url;
 	}
 
-	public void setImgUrl(String imgUrl) {
-		this.imgUrl = imgUrl;
+
+	public void setImg_url(String img_url) {
+		this.img_url = img_url;
 	}
+
 
 	public String getShortDescription() {
 		return shortDescription;
 	}
 
-	public void setShortDescription(String shortDescription) {
-		this.shortDescription = shortDescription;
+
+	public void setShortDescription(String short_description) {
+		this.shortDescription = short_description;
 	}
+
 
 	public String getLongDescription() {
 		return longDescription;
 	}
 
-	public void setLongDescription(String longDescription) {
-		this.longDescription = longDescription;
+
+	public void setLongDescription(String long_description) {
+		this.longDescription = long_description;
 	}
+
 
 	@Override
 	public int hashCode() {
